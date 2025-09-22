7bcb9744935e2bd21dc7f3e42c004275b3a8f15e
Hitesh ahire <hbahire2014@gmail.com>
2025-09-22 21:29:32 +0530

    frequency of word code.

---
+++ src/java8/FrequencyOfEachWord.java
@@ -0,0 +1,21 @@
+//4) How do you find frequency of each element in an array or a list?
+package java8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FrequencyOfEachWord {
+    public static void main(String[] args) {
+        List<String> stringList = Arrays.asList("Pen", "Eraser", "Note Book", "Pen", "Pencil", "Stapler", "Note Book", "Pencil");
+
+        Map<String, Long> result = stringList
+                .stream()
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        System.out.println("result - " + result);
+
+    }
+}
