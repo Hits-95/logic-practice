47e1cf18576295fad1c066e0de966524f860d745
Hitesh ahire <hbahire2014@gmail.com>
2025-09-28 23:24:57 +0530

    Fix rotateString logic to correctly check string rotations

---
+++ src/String/RotationOfString.java
@@ -0,0 +1,19 @@
+package String;
+
+public class RotationOfString {
+
+    public static void main(String[] args) {
+        String s = "abcde";
+        String goal = "abced";
+
+        System.out.println(rotateString(s, goal)); // ✅ true
+    }
+
+    public static boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        String concatenated = s + s;
+        return concatenated.contains(goal);
+    }
+}
